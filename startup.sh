488d30c17face1bd16d00ffa7af07a493c659ece
Elizabeth <foxcapade@gmail.com>
2023-04-19 14:29:00 -0400

    update startup script

--- startup.sh
+++ startup.sh
@@ -5,7 +5,6 @@ export KAFKA_SERVER_PROPERTIES_FILE="${KAFKA_SERVER_PROPERTIES_FILE:-/opt/kafka/
 # If the properties file already exists
 if [ -f "${KAFKA_SERVER_PROPERTIES_FILE}" ]; then
   echo "Mounted Kafka properties file found."
-  exit 0
 else
   echo "No mounted Kafka properties file found, generating one."
 
@@ -15,12 +14,12 @@ else
   . server.properties.sh
 
   build_properties
-fi
 
-if [ -z "${KAFKA_CLUSTER_ID}" ]; then
-  KAFKA_CLUSTER_ID="$(kafka-storage.sh random-uuid)"
-fi
+  if [ -z "${KAFKA_CLUSTER_ID}" ]; then
+    KAFKA_CLUSTER_ID="$(kafka-storage.sh random-uuid)"
+  fi
 
-kafka-storage.sh format -t "$KAFKA_CLUSTER_ID" -c "${KAFKA_SERVER_PROPERTIES_FILE}"
+  kafka-storage.sh format -t "$KAFKA_CLUSTER_ID" -c "${KAFKA_SERVER_PROPERTIES_FILE}"
+fi
 
 kafka-server-start.sh "${KAFKA_SERVER_PROPERTIES_FILE}"
